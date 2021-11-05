45e288eaeeba26be0718ac6aa997460b343065a3
Vic1506 <78886523+Vic1506@users.noreply.github.com>
2021-11-04 20:53:35 -0700

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -7,8 +7,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=' . $dbname, $username, $password);
 if (isset($_POST['colum1'])) {
   $id=$_POST['colum1'];
   $query = "DELETE FROM table1 WHERE column1=:id;";
- //echo $query;
-  //$query = "DELETE FROM table1 WHERE column1=:id;";
+
 
   $stmt = $pdo->prepare($query);
   $stmt->bindParam(":id", $id);
@@ -18,6 +17,5 @@ if (isset($_POST['colum1'])) {
   echo "Algo salio mal <a href='https://localhost/crud/crud.php'> clic aqui para volver al crud</a>" ;
 
 }
- //echo $query
   
 ?>
